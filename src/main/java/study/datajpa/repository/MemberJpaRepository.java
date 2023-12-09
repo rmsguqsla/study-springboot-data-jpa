236d470e0a5e9395385bd2766b9f268212299a6a
Oh Keun-Hyeop <rmsguqsla@gmail.com>
2023-12-09 17:26:52 +0900

    순수 JPA 페이징과 정렬

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -46,4 +46,26 @@ public class MemberJpaRepository {
                 .setParameter("age", age)
                 .getResultList();
     }
+
+    // offset : 몇 번째 부터, limit : 몇 개를 가져와
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery(
+                "select m " +
+                        "from Member m " +
+                        "where m.age = :age " +
+                        "order by m.username desc")
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery(
+                "select count(m) " +
+                        "from Member m " +
+                        "where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -32,5 +32,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findListByUsername(String username); //컬렉션
     Member findMemberByUsername(String username); //단건
     Optional<Member> findOptionalByUsername(String username); //단건 Optional
+
+
 }
 
